b9a3f2a9a87f58ff13c7b341f9696e21ee071784
LeakyMon <112800791+LeakyMon@users.noreply.github.com>
2022-11-15 01:09:26 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,9 +8,13 @@ using namespace std;
 void numLetters(string word, ofstream& output) {
 
 	string temp = "";
+	
+
 
 	for (int i = 0; i <= word.size(); i++) {
 		
+		
+
 		if (i == word.size()){
 			output << "<b>" << "</b>" << temp << " ";
 		}
@@ -85,20 +89,26 @@ void numLetters(string word, ofstream& output) {
 }
 
 int main() {
-
+	
 	ifstream inFile;
-	ofstream product("output.txt");
-	inFile.open("FILENAME_HERE.txt");
+	ofstream product("output.html");
+	inFile.open("Bionic.txt");
 
 	if (inFile.is_open()) {
 		cout << "Open" << endl;
 	}
 
 	string temp;
-	int x = 0;
+	product << "<!DOCTYPE html>";
+	product << "<html lang = \"en\">";
+	product << "<head> <meta charset=\"UTF-8\"> <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">  <title>Bionic Reading Output</title></head><body>";
+	
 	while (getline(inFile, temp)) {
+		
 		numLetters(temp, product);
 		product << "<br>" << endl;
+		
 	}
+
 	return 0;
 }
